1546d0ef8156bff1f5483c32215eece65870641f
Jimwel Dizon <jimweldizon.dev@gmail.com>
2024-07-08 16:30:53 +0800

    fix runtime error

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersModule } from 'src/users/users.module';
-import configuration from 'src/config/configuration';
+import { UsersModule } from '../users/users.module';
+import configuration from '../config/configuration';
 
 @Module({
   imports: [
