931e0d9786f50861d1a209aacf175063a90b176d
Jimwel Dizon <jimweldizon.dev@gmail.com>
2024-07-08 21:50:42 +0800

    vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import configuration from './config/configuration';
 import cookieParser from 'cookie-parser';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,7 @@ async function bootstrap() {
     credentials: true,
   });
 
+  app.useWebSocketAdapter(new IoAdapter(app));
   await app.listen(configuration().app.port);
 
   console.log(`Application is running`);
